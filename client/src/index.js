cba9615002c785444a4fd7e44cba148fe70628c2
sarthak2991 <99971932+sarthak2991@users.noreply.github.com>
2023-10-17 00:14:15 +0530

    Create index.js

---
+++ client/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+import Desktop from "./Desktop";
+import Desktop3 from "./Desktop3";
+import Desktop4 from "./Desktop4";
+import Desktop5 from "./Desktop5";
+import Desktop6 from "./Desktop6";
+import Desktop7 from "./Desktop7";
+import Desktop8 from "./Desktop8";
+import Login from "./Login";
+
+
+function App (){
+    return (
+        <>
+       
+       
+        </>
+        )
+    
+}
+
+ReactDOM.render(<App/>,document.getElementById('root'))
