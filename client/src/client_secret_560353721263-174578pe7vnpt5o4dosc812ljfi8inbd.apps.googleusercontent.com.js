c07a30f2a376a230b158fd292e0c3fa93bae2a43
sarthak2991 <99971932+sarthak2991@users.noreply.github.com>
2023-10-23 02:05:40 +0530

    Delete client/src/client_secret_560353721263-174578pe7vnpt5o4dosc812ljfi8inbd.apps.googleusercontent.com.js

--- client/src/client_secret_560353721263-174578pe7vnpt5o4dosc812ljfi8inbd.apps.googleusercontent.com.js
+++
@@ -1,4 +0,0 @@
-const data = {web:{"client_id":"560353721263-174578pe7vnpt5o4dosc812ljfi8inbd.apps.googleusercontent.com","project_id":"expanded-system-388318","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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","redirect_uris":["http://localhost:3000"],"javascript_origins":["http://localhost:3000"]}}
-
-export default data
-
